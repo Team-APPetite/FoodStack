079e36f00c1f07caf4c5359f90b9d3cf0facc7a1
nicolej2122 <nicole.joseph2122@gmail.com>
2021-05-25 01:10:38 +0800

    Updates to Home Screen UI

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -10,105 +10,122 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Column(children: [
-        Text(
-          'Welcome to FoodStack',
-          style: TextStyle(
-            fontFamily: 'Avenir',
-            fontSize: 40.0,
-            fontWeight: FontWeight.bold,
-            color: ThemeColors.dark,
-          ),
-        ),
-        Row(
-          children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: Padding(
-                padding:
-                const EdgeInsets.symmetric(horizontal: 80.0, vertical: 16.0),
-                child: Text(
-                  'Start a new order',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                primary: ThemeColors.oranges,
+      body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(children: [
+            SizedBox(height: 100.0),
+            Text(
+              'Welcome to FoodStack',
+              style: TextStyle(
+                fontFamily: 'Avenir',
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: ThemeColors.dark,
               ),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Padding(
-                padding:
-                const EdgeInsets.symmetric(horizontal: 80.0, vertical: 16.0),
-                child: Text(
-                  'Join nearby orders',
-                  style: TextStyle(
-                    fontSize: 16.0,
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 300.0),
+                ConstrainedBox(
+                  constraints: BoxConstraints.tightFor(width: 200, height: 200),
+                  child: ElevatedButton.icon(
+                  onPressed: () {},
+
+                   icon: Icon(Icons.add_shopping_cart),
+
+                   label: Text(
+                      'Start a New Order',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                     ),
+                    primary: ThemeColors.oranges,
+                  ),
                   ),
                 ),
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
+                ConstrainedBox(
+                  constraints: BoxConstraints.tightFor(width: 200, height: 200),
+                  child: ElevatedButton.icon(
+                  onPressed: () {},
+                    icon: Icon(Icons.person_add_alt_1_outlined),
+                    label: Text(
+                      'Join Nearby Orders',
+                       textAlign: TextAlign.center,
+                       style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    primary: ThemeColors.oranges,
+                  ),
                 ),
-                primary: ThemeColors.oranges,
-              ),
+                ),
+             ]
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 300.0),
+                ConstrainedBox(
+                  constraints: BoxConstraints.tightFor(width: 200, height: 200),
+                  child: ElevatedButton.icon(
+                      onPressed: () {},
+                        icon: Icon(Icons.favorite_border_outlined),
+                        label: Text(
+                          'Favourites',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+
+                      style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      ),
+                        primary: ThemeColors.oranges,
+                      ),
+                  ),
+                ),
+
+                ConstrainedBox(
+                  constraints: BoxConstraints.tightFor(width: 200, height: 200),
+                  child:ElevatedButton.icon(
+                    onPressed: () {},
+                      icon: Icon(Icons.access_time),
+                      label: Text(
+                        'Recent Orders',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                         fontSize: 16.0,
+                      ),
+                    ),
+
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    primary: ThemeColors.oranges,
+                  ),
+                  ),
+                ),
+              ],
             ),
           ],
-        ),
-        Row(
-          children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: Padding(
-                padding:
-                const EdgeInsets.symmetric(horizontal: 80.0, vertical: 16.0),
-                child: Text(
-                  'Favourites',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                primary: ThemeColors.oranges,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Padding(
-                padding:
-                const EdgeInsets.symmetric(horizontal: 80.0, vertical: 16.0),
-                child: Text(
-                  'Place your recent orders',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                primary: ThemeColors.oranges,
-              ),
-            ),
-          ],
-        ),
-          ],
-        )
-        ),
+          )
+      ),
     );
   }
 }

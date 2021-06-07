2acabbaddeadccafcca2678c580065ee19f95f98
nicolej2122 <nicole.joseph2122@gmail.com>
2021-06-07 22:38:04 +0800

    User info database

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -1,7 +1,33 @@
-class User {
+import 'package:flutter/cupertino.dart';
+
+class Users {
+  String uid;
   String name;
   String email;
   String address;
 
-  User({this.name, this.email, this.address});
+  Users({
+    @required this.uid,
+    this.name,
+    this.email,
+    this.address,
+  });
+
+  Users.fromJson(Map<String, dynamic> json) {
+    uid = json['uid'];
+    name = json['name'];
+    email = json['email'];
+    address = json['address'];
+  }
+
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['uid'] = this.uid;
+    data['name'] = this.name;
+    data['email'] = this.email;
+    data['address'] = this.address;
+
+    return data;
+  }
 }

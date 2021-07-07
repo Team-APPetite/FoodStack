08bf31a02eb421b6d71fa45cc6229b2216c399d2
Charisma Kausar <68203159+ckcherry23@users.noreply.github.com>
2021-07-07 13:43:27 +0300

    Allow pull-to-refresh on favourites page

--- lib/src/screens/favourites.dart
+++ lib/src/screens/favourites.dart
@@ -11,33 +11,51 @@ class FavouritesScreen extends StatefulWidget {
 }
 
 class _FavouritesScreenState extends State<FavouritesScreen> {
+  Stream<List<Restaurant>> favouritesList;
+
+  @override
+  void initState() {
+    final restaurantProvider =
+        Provider.of<RestaurantProvider>(context, listen: false);
+    favouritesList = restaurantProvider.getFavouriteRestaurants();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-  final restaurantProvider = Provider.of<RestaurantProvider>(context);
+    final restaurantProvider = Provider.of<RestaurantProvider>(context);
 
-    // TODO Add search bar
     return Scaffold(
         appBar: Header.getAppBar(title: 'Your Favourites'),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: StreamBuilder<List<Restaurant>>(
-              stream: restaurantProvider.getFavouriteRestaurants(),
+              stream: favouritesList,
               builder: (context, snapshot) {
                 if (snapshot.data == null) {
                   return Center(child: CircularProgressIndicator());
                 } else {
                   return Scrollbar(
-                    child: ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (context, index) {
-                          return RestaurantCard(
-                              snapshot.data[index].restaurantId,
-                              snapshot.data[index].restaurantName,
-                              snapshot.data[index].cuisineType,
-                              snapshot.data[index].deliveryFee,
-                              snapshot.data[index].rating,
-                              snapshot.data[index].image,
-                              favourite: true);
+                    child: RefreshIndicator(
+                        child: ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index) {
+                              return RestaurantCard(
+                                  snapshot.data[index].restaurantId,
+                                  snapshot.data[index].restaurantName,
+                                  snapshot.data[index].cuisineType,
+                                  snapshot.data[index].deliveryFee,
+                                  snapshot.data[index].rating,
+                                  snapshot.data[index].image,
+                                  favourite: true);
+                            }),
+                        onRefresh: () {
+                          return Future.delayed(Duration(seconds: 1), () {
+                            setState(() {
+                              favouritesList =
+                                  restaurantProvider.getFavouriteRestaurants();
+                            });
+                          });
                         }),
                   );
                 }

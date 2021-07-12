31dbd2c3c8735941d4a3161ee994a1ec22c13060
Charisma Kausar <68203159+ckcherry23@users.noreply.github.com>
2021-07-12 19:31:52 +0300

    Add filter chips for cuisine types

--- lib/src/screens/newOrder.dart
+++ lib/src/screens/newOrder.dart
@@ -1,6 +1,9 @@
+import 'package:chips_choice/chips_choice.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:foodstack/src/models/restaurant.dart';
+import 'package:foodstack/src/styles/textStyles.dart';
+import 'package:foodstack/src/styles/themeColors.dart';
 import 'package:foodstack/src/widgets/header.dart';
 import 'package:foodstack/src/widgets/restaurantCard.dart';
 import 'package:foodstack/src/providers/restaurantProvider.dart';
@@ -9,7 +12,19 @@ import 'package:provider/provider.dart';
 import 'package:foodstack/src/providers/userLocator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+// ignore: must_be_immutable
 class NewOrderScreen extends StatefulWidget {
+  bool isFilterCardShown = false;
+  bool areFiltersEnabled = false;
+
+  List<String> tags = [];
+  List<String> options = [
+    'Casual',
+    'Fast Food',
+    'Desserts',
+    'Beverages',
+    'Healthy',
+  ];
   @override
   _NewOrderScreenState createState() => _NewOrderScreenState();
 }
@@ -30,6 +45,47 @@ class _NewOrderScreenState extends State<NewOrderScreen> {
   Widget build(BuildContext context) {
     final restaurantProvider = Provider.of<RestaurantProvider>(context);
     final userLocator = Provider.of<UserLocator>(context);
+
+    Widget _filterCard() {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Cuisine Type',
+                style: TextStyles.heading3(),
+                textAlign: TextAlign.start,
+              ),
+              ChipsChoice<String>.multiple(
+                value: widget.tags,
+                onChanged: (val) => setState(() {
+                  widget.tags = val;
+                  if (widget.tags.isNotEmpty) {
+                    widget.areFiltersEnabled = true;
+                  } else {
+                    widget.areFiltersEnabled = false;
+                  }
+                }),
+                choiceItems: C2Choice.listFrom<String, String>(
+                  source: widget.options,
+                  value: (i, v) => v,
+                  label: (i, v) => v,
+                ),
+                wrapCrossAlignment: WrapCrossAlignment.start,
+                choiceStyle: C2ChoiceStyle(),
+                choiceActiveStyle: C2ChoiceStyle(
+                    showCheckmark: true,
+                    color: ThemeColors.oranges,
+                    labelStyle: TextStyle(color: Colors.white),
+                    brightness: Brightness.dark),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
 
     // TODO Add search bar
     return Scaffold(
@@ -58,10 +114,34 @@ class _NewOrderScreenState extends State<NewOrderScreen> {
                     children: [
                       Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: CupertinoSearchTextField(
-                          padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: CupertinoSearchTextField(
+                                padding: EdgeInsets.all(15.0),
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            InkResponse(
+                              child: Icon(
+                                Icons.filter_list,
+                                size: 30,
+                                color: widget.areFiltersEnabled
+                                    ? ThemeColors.oranges
+                                    : ThemeColors.dark,
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  widget.isFilterCardShown =
+                                      !widget.isFilterCardShown;
+                                });
+                              },
+                              radius: 20,
+                            ),
+                          ],
                         ),
                       ),
+                      widget.isFilterCardShown ? _filterCard() : Container(),
                       Expanded(
                         child: Scrollbar(
                           child: ListView.builder(

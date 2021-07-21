4615cb29302b6385f8caef9f06e56ed4063a4b2e
Charisma Kausar <68203159+ckcherry23@users.noreply.github.com>
2021-07-22 01:26:49 +0300

    Add null cases for price calculation

--- lib/src/utilities/priceCalculation.dart
+++ lib/src/utilities/priceCalculation.dart
@@ -1,19 +1,26 @@
+import 'dart:math';
+
 import 'package:foodstack/src/utilities/numbers.dart';
 
 class PriceCalculation {
-
   static double finalDeliveryFee(double deliveryFee, int numOfUsers) {
-    double finalDeliveryFee = deliveryFee/numOfUsers;
-    return Numbers.roundTo2d(finalDeliveryFee);
+    if (deliveryFee != null) {
+      double finalDeliveryFee = deliveryFee / numOfUsers;
+      return Numbers.roundTo2d(finalDeliveryFee);
+    } else {
+      return 0;
+    }
   }
 
-  static double totalFee(double _subtotal, double _deliveryFee, int _numOfUsers) {
-   double _totalFee = 0.0;
-   double _finalDeliveryFee = finalDeliveryFee(_deliveryFee, _numOfUsers);
-   _totalFee = _subtotal + _finalDeliveryFee;
-   return Numbers.roundTo2d(_totalFee);
+  static double totalFee(
+      double _subtotal, double _deliveryFee, int _numOfUsers) {
+    double _totalFee = 0.0;
+    double _finalDeliveryFee = finalDeliveryFee(_deliveryFee, _numOfUsers);
+    if (_finalDeliveryFee != 0) {
+      _totalFee = _subtotal + _finalDeliveryFee;
+      return Numbers.roundTo2d(_totalFee);
+    } else {
+      return 0;
+    }
   }
-
-
-
 }
